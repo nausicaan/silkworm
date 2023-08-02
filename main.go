470b2f9a58701423abf2fbf7be7c5279404a5558
Byron Stuike <byron.stuike@gov.bc.ca>
2023-08-02 16:31:39 -0700

    bcgov-plugin/gravityforms:2.7.11 (DESSO-1332)

--- main.go
+++ main.go
@@ -12,20 +12,43 @@ const (
 	reset  string = "\033[0m"
 	green  string = "\033[32m"
 	yellow string = "\033[33m"
-	zero   string = "Insufficient arguments supplied -"
+	red    string = "\033[41m"
+	halt   string = "program halted"
 )
 
 // Launch the program and execute according to the supplied flag
 func main() {
-	switch os.Args[1] {
+	trigger := verify()
+	if len(trigger) > 25 {
+		trigger = "-g"
+	}
+	switch trigger {
 	case "-v", "--version":
 		fmt.Println(yellow+"Silkworm", green+bv)
 		fmt.Println(reset)
 	case "-h", "--help":
 		about()
-	default:
+	case "-z":
+		alert("No arguments detected -")
+		about()
+	case "-g":
 		workers.Quarterback()
+	default:
+		alert("Unknown argument(s) supplied -")
+		about()
 	}
+}
+
+// Test for the minimum number of arguments
+func verify() string {
+	passed := len(os.Args)
+	var f string
+	if passed == 1 {
+		f = "-z"
+	} else {
+		f = os.Args[1]
+	}
+	return f
 }
 
 // about prints help information for using the program
@@ -33,7 +56,7 @@ func about() {
 	fmt.Println(yellow, "\nUsage:", reset)
 	fmt.Println("  [program] [vendor/plugin]:[version]")
 	fmt.Println(yellow, "\nExample:", reset)
-	fmt.Println("  Against your composer.json file, run:")
+	fmt.Println("  Adding your path to file, if necessary, run:")
 	fmt.Println(green + "    silkworm wpackagist-plugin/mailpoet:4.6.1")
 	fmt.Println(yellow, "\nAdditional Options:")
 	fmt.Println(green, " -h, --help", reset, "		Help Information")
@@ -42,4 +65,9 @@ func about() {
 	fmt.Println("  For more information go to:")
 	fmt.Println(green, "   https://github.com/nausicaan/silkworm.git")
 	fmt.Println(reset)
+}
+
+// Alert prints a colourized error message
+func alert(message string) {
+	fmt.Println("\n"+red, message, halt, reset)
 }
